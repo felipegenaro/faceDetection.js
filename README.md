8e54b4075dfaa1b6fda2fc9642994d50ca37f079
Felipe Corniani de Genaro <felipegenaro@outlook.com.br>
2021-02-24 17:06:15 -0300

    first steps with facedetection api

---
+++ README.md
@@ -0,0 +1,11 @@
+# Face Detection
+
+## The face-api is a wrapper around TensorFlow
+### Use the Live Server extension for VS Code or just:
+``` bash
+        $ npm install face-api.js
+```
+### Or
+``` bash
+        $ npm install face-api.js canvas @tensorflow/tfjs-node
+```
---
+++ script.js
@@ -0,0 +1,42 @@
+const selector = selector => document.querySelector(selector);
+
+const video = selector('#video')
+
+// get webcam content
+const startVideo = () => {
+  navigator.getUserMedia(
+    { video: {} },
+    stream => video.srcObject = stream,
+    err => console.error(err)
+  )
+}
+
+// load models
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+// live recognition
+video.addEventListener('playing', () => {
+  const canvas = faceapi.createCanvasFromMedia(video);
+  document.body.append(canvas);
+
+  const displaySize = { width: video.width, height: video.height };
+  faceapi.matchDimensions(canvas, displaySize);
+
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
+    const resizedDetections = faceapi.resizeResults(detections, displaySize);
+
+    // generate canvas
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
+
+    // draw models on screen
+    faceapi.draw.drawDetections(canvas, resizedDetections);
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections);
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections);
+  }, 100)
+})
